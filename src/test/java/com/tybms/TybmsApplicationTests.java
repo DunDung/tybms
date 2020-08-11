dc00a557f38f2785255c57ec79798b519c1cdff3
JunSeong <ebseud6135@naver.com>
2020-08-11 19:07:38 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/tybms/TybmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tybms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TybmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
