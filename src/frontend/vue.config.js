642bfd002fa978a966adb19c75fedd02d5c585f0
JunSeong <ebseud6135@naver.com>
2020-09-21 16:56:45 +0900

    fix: gradle 빌드 오류 수정

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    outputDir: path.resolve("__dirname", "../main/resources/static"),
+    outputDir: path.resolve("__dirname", "../src/main/resources/static"),
     devServer: {
         proxy: {
             '/': {
