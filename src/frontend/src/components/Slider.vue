7c44255e977ca43c02f21d9aa5966a8835361ebe
JunSeong <ebseud6135@naver.com>
2020-11-30 23:09:08 +0900

    feat: top header 구현

--- src/frontend/src/components/Slider.vue
+++ src/frontend/src/components/Slider.vue
@@ -1,29 +1,31 @@
 <template>
-  <v-carousel
-    cycle
-    height="100%"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item v-for="(slide, i) in slides" :key="i">
-      <v-sheet height="80%">
-        <v-row class="fill-height" align="center" justify="center">
-          <div class="display-3">
-            <img :src="slide" />
-          </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
+  <v-main>
+    <v-carousel
+      cycle
+      max-height="100%"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet max-height="80%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="display-3">
+              <v-img :src="slide" max-width="100%" height="auto" />
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </v-main>
 </template>
 <script>
 export default {
   data() {
     return {
       slides: [
-        require("@/assets/main/main_img01.jpg"),
-        require("@/assets/main/main_img02.jpg"),
-        require("@/assets/main/main_img03.jpg")
+        require("@/assets/main/main-img01.jpg"),
+        require("@/assets/main/main-img02.jpg"),
+        require("@/assets/main/main-img03.jpg")
       ]
     };
   }
--- src/frontend/src/views/Header.vue
+++ src/frontend/src/views/Header.vue
@@ -1,8 +1,13 @@
 <template>
-  <Slider />
+  <v-main>
+    <img width="100%" src="@/assets/header/top-header2.jpg" />
+    <h1>동양 BMS</h1>
+    <Slider />
+  </v-main>
 </template>
 <script>
 import Slider from "@/components/Slider";
+
 export default {
   components: {
     Slider
