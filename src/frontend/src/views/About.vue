a4744015ab63fe36e4dfe266b1cff1afd2d944d6
JunSeong <ebseud6135@naver.com>
2020-12-03 21:12:06 +0900

    feat: 회사 소개 페이지 About.vue 구현

---
+++ src/frontend/src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <SubMenu :subMenus="aboutSubMenus" />
+    <Story v-if="aboutSubMenus[0].isActive" />
+  </v-app>
+</template>
+<script>
+import SubMenu from "@/components/SubMenu";
+import Story from "@/components/Story";
+
+export default {
+  data() {
+    return {
+      aboutSubMenus: [
+        {
+          title: "동양비엠에스 스토리",
+          isActive: true
+        },
+        {
+          title: "동양비엠에스 연혁",
+          isActive: false
+        },
+        {
+          title: "찾아오시는 길",
+          isActive: false
+        }
+      ]
+    };
+  },
+  components: {
+    SubMenu,
+    Story
+  }
+};
+</script>
+<style scoped></style>
