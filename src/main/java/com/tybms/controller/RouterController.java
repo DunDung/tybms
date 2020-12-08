44f14a9c4b721f6b1bc0003adc2e2f39a56287bd
JunSeong <ebseud6135@naver.com>
2020-12-08 19:05:34 +0900

    feat: SPA GET 요청을 위한 RouterController 추가

--- src/main/java/com/tybms/controller/RouterController.java
+++ src/main/java/com/tybms/controller/RouterController.java
@@ -1,12 +1,21 @@
 package com.tybms.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@RestController
-public class RouterController {
-    @GetMapping
-    public String hello() {
-        return "hello";
+@Controller
+public class RouterController implements ErrorController {
+    @GetMapping({"/", "/error"})
+    @ResponseStatus(HttpStatus.OK)
+    public String index() {
+        return "index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
