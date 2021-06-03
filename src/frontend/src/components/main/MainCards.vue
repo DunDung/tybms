9a689110cd910afce43c0e81a46bfaa62ee70d8d
JunSeong <ebseud6135@naver.com>
2021-06-04 01:47:48 +0900

    refactor: 메인 페이지 하단 height 수정 및 미니 슬라이더 이미지 교체

--- src/frontend/src/components/main/MainCards.vue
+++ src/frontend/src/components/main/MainCards.vue
@@ -4,7 +4,13 @@
       <v-carousel cycle hide-delimiter-background interval="3000" height="100%">
         <v-carousel-item v-for="(slide, i) in slides" :key="i">
           <v-row class="fill-height" align="center" justify="center">
-            <img :src="slide" />
+            <picture>
+              <source :srcset="slide.w320" media="(max-width: 400px)" />
+              <source :srcset="slide.w640" media="(max-width: 640px)" />
+              <source :srcset="slide.w960" media="(max-width: 960px)" />
+              <source :srcset="slide.w320" media="(max-width: 1920px)" />
+              <img :src="slide.w320" />
+            </picture>
           </v-row>
         </v-carousel-item>
       </v-carousel>
@@ -31,6 +37,19 @@
           <li>
             <router-link to="/#">공지사항 8</router-link>
           </li>
+          <li>
+            <router-link to="/#">공지사항 7</router-link>
+          </li>
+          <li>
+            <router-link to="/#">공지사항 6</router-link>
+          </li>
+          <li>
+            <router-link to="/#">공지사항 5</router-link>
+          </li>
+          <li>
+            <router-link to="/#">공지사항 4</router-link>
+          </li>
+
         </ul>
       </section>
     </v-card>
@@ -56,6 +75,18 @@
           <li>
             <router-link to="#">제 8기 주주총회 안내문 </router-link>
           </li>
+          <li>
+            <router-link to="#">제 7기 주주총회 안내문 </router-link>
+          </li>
+          <li>
+            <router-link to="#">제 6기 주주총회 안내문 </router-link>
+          </li>
+          <li>
+            <router-link to="#">제 5기 주주총회 안내문 </router-link>
+          </li>
+          <li>
+            <router-link to="#">제 4기 주주총회 안내문 </router-link>
+          </li>
         </ul>
       </section>
     </v-card>
@@ -74,15 +105,26 @@
 <script>
 export default {
   data: () => ({
-    slides: [require("@/assets/images/main/mini-slider/home-mini-slider01.png")]
+    slides: [
+      {
+        w320: require("@/assets/images/main/mini-slider/mini-slider-1-320.png"),
+        w640: require("@/assets/images/main/mini-slider/mini-slider-1-640.png"),
+        w960: require("@/assets/images/main/mini-slider/mini-slider-1-960.png"),
+      },
+      {
+        w320: require("@/assets/images/main/mini-slider/mini-slider-2-320.png"),
+        w640: require("@/assets/images/main/mini-slider/mini-slider-2-640.png"),
+        w960: require("@/assets/images/main/mini-slider/mini-slider-2-960.png"),
+      }
+    ]
   })
 };
 </script>
 
 <style scoped>
 .wrap {
-  margin-top: 2rem;
-  margin-bottom: 2rem;
+  margin-top: 2.3rem;
+  margin-bottom: 2.3rem;
   display: flex;
   flex-direction: row;
   width: 100%;
@@ -92,7 +134,7 @@ export default {
 .cards {
   font-family: "NanumSquare", sans-serif !important;
   width: 25%;
-  height: 200px;
+  height: 350px;
 }
 
 .cards-margin-left {
@@ -100,8 +142,10 @@ export default {
 }
 
 img {
-  height: 100%;
   width: 100%;
+  height: 100%;
+  object-fit: fill;
+
 }
 
 .youtube-frame {
@@ -115,12 +159,12 @@ li {
 }
 
 li {
-  border-bottom: 1px dashed #666;
+  border-bottom: 1px dashed black;
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
   padding-bottom: 3px;
-  margin-bottom: 10px;
+  margin-bottom: 13px;
 }
 
 li::before {
@@ -128,23 +172,24 @@ li::before {
   display: inline-block;
   width: 5px;
   height: 5px;
-  background-color: #666;
+  background-color: black;
   border-radius: 100%;
   line-height: 2;
+  margin-right: 5px;
 }
 
 a {
   text-decoration: none;
-  color: #ffffff;
+  color: blue;
   font-weight: bold;
 }
 
 .notice {
-  margin-top: 1.6vh;
   position: relative;
   width: 100%;
   border: 1px solid #000;
   padding: 30px 10px 0;
+  height: 100%;
 }
 
 h2 {
@@ -168,7 +213,7 @@ h2 {
 
 .youtube-frame {
   width: 100%;
-  height: 200px;
+  height: 100%;
 }
 
 @media screen and (max-width: 960px) {
