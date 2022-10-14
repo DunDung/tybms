46fad6c5da910e4c2b9988657d6baa6ed71322b7
홍준성 <dundung@woowahan.com>
2022-10-15 00:34:33 +0900

    정적 리소스 캐시 적용

--- src/main/java/co/kr/tybms/config/CacheType.java
+++ src/main/java/co/kr/tybms/config/CacheType.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CacheType {
-
     MATERIALS(Integer.MAX_VALUE, 100),
     NOTICES(Integer.MAX_VALUE, 100),
     PRODUCTS(Integer.MAX_VALUE, 100);
